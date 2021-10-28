157a828f2fffa578c27215c5074270c2fa519d0b
解犴 <784504882@qq.com>
2021-10-28 18:05:30 +0800

    登陆注册、基本样式

--- logic.py
+++ logic.py
@@ -1,12 +1,33 @@
-from flask import Flask, render_template, request
+# 逻辑处理单元 #
+
+from flask import Flask, render_template, request, url_for
 import pymysql
 
 conn = pymysql.connect(host="localhost", user="root", password="2012shijiemori", database="film", charset="utf8")
 
 cursor = conn.cursor()
 
-def get_show():
-    return render_template('register.html')
+
+def index_ori():
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        # 中文查找是什么电影
+        film_name = request.form['search_film']
+        sql_film_name = 'select * from film.film where film_name_ch' + '=' + '%s'
+        cursor.execute(sql_film_name, [film_name])
+
+        film_infos = cursor.fetchall()
+
+        for film_info in film_infos:
+            film_imdb = film_info[0]
+            film_name_ch = film_info[1]
+            film_name_en = film_info[2]
+
+        if film_imdb:
+            # return url_for('film', imdb=film_imdb)
+            return render_template('film.html', film_imdb=film_imdb, film_name_en=film_name_en)
+    return render_template('index.html')
 
 def get_login():
     if request.method == 'GET':
@@ -22,15 +43,60 @@ def get_login():
 
         for user in usernames:
             if request.form['username'] == user[0]:
-                sql_pw = 'select user_password from film.user where user_name'+'='+'%s'
+                sql_pw = 'select user_password from film.user where user_name' + '=' + '%s'
                 cursor.execute(sql_pw, (user[0]))
 
                 pw = cursor.fetchall()
 
-                # if request.form['password'] == pw[0][0]:
-                if request.form['password'] == pw:
-                    return '<h>欢迎回来，%s!</h>' % user[0]
+                if request.form['password'] == pw[0][0]:
+                    # if request.form['password'] == pw:
+                    return render_template('index.html', user_name=username)
 
                 return '<h>账号密码错误！</h>'
         cursor.close()
         conn.close()
+
+
+def get_register():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        user_name = request.form['username']
+        user_password = request.form['password']
+        if len(user_name) == 0 | len(user_password) == 0:
+            return render_template('register.html')
+        insert_user(user_name, user_password)
+
+    return render_template("register.html")
+
+def get_film(imdb):
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        # 中文查找是什么电影
+        film_name = request.form['search_film']
+        sql_film_name = 'select * from film.film where film_name_ch' + '=' + '%s'
+        cursor.execute(sql_film_name, [film_name])
+
+        film_infos = cursor.fetchall()
+
+        for film_info in film_infos:
+            film_imdb = film_info[0]
+            film_name_ch = film_info[1]
+            film_name_en = film_info[2]
+
+        if film_imdb:
+            # return url_for('film', imdb=film_imdb)
+            return render_template('film.html', film_imdb=film_imdb, film_name_en=film_name_en)
+
+# 插入操作
+def insert_user(user_name, user_password):
+    sql_insert_user = 'insert into film.user values(' + '%s' + ', %s' + ')'
+    try:
+        cursor.execute(sql_insert_user, [user_name, user_password])
+        #     提交
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+
+    cursor.close()
---
+++ views.py
@@ -0,0 +1,82 @@
+# 接口逻辑单元 #
+
+
+from flask import Flask, render_template, request, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, EqualTo
+from flask_sqlalchemy import SQLAlchemy
+
+import logic
+
+# 创建Flask对象app并初始化
+app = Flask(__name__)  # __name__的作用是为了确认资源所在的路径
+app.secret_key = 'xiejunan'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    show_data = logic.index_ori()
+    return show_data
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    login_data = logic.get_login()
+    return login_data
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    register_data = logic.get_register()
+    return register_data
+
+
+@app.route("/films/<int:imdb>", methods=["GET", "POST"])
+def film():
+    film_data = logic.get_film()
+    return film_data
+
+
+
+
+    # 查询操作
+    # sql = "SELECT * FROM Cast"
+    # try:
+    #     cursor.execute(sql)
+    #
+    #     results = cursor.fetchall()
+    #
+    #     for row in results:
+    #         person_name = row[3]
+    #         cast_type = row[2]
+    #         # print("Big boy is %s, %s." % (person_name, cast_type))
+    #         return render_template("index.html", person_name=person_name)
+    # except:
+    #     print("Error!")
+    # cursor.close()
+
+
+    # 更新操作
+    # sql_update_film = "update film.Cast set person_name = '%s' where person_id = %d"
+    # sql_update_film2= "update film.Cast set cast_type = '%s' where person_id = %d "
+    #
+    # try:
+    #     cursor.execute(sql_update_film % ("Nobody", 2))
+    #     cursor.execute(sql_update_film2 % ("actor", 2))
+    #     conn.commit()
+    # except Exception as e:
+    #     conn.rollback()
+
+    # 删除操作
+    # sql_delete= "delete from film.Cast where person_id = %d"
+    #
+    # try:
+    #     cursor.execute(sql_delete % (2))
+    #     conn.commit()
+    # except Exception as e:
+
+
+#     conn.rollback()
+
+
