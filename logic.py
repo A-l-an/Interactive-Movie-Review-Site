5447277da371c0aac1f51026411addbd4f34fea4
解犴 <784504882@qq.com>
2021-11-01 10:11:13 +0800

    没尝试cookies的版本

--- logic.py
+++ logic.py
@@ -12,37 +12,23 @@ def index_ori():
     if request.method == 'GET':
         return render_template('index.html')
     else:
-        # 中文查找是什么电影
-        film_name = request.form['search_film']
-        sql_film_name = 'select * from film.film where film_name_ch' + '=' + '%s'
-        cursor.execute(sql_film_name, [film_name])
+        return get_film()
 
-        film_infos = cursor.fetchall()
-
-        for film_info in film_infos:
-            film_imdb = film_info[0]
-            film_name_ch = film_info[1]
-            film_name_en = film_info[2]
-
-        if film_imdb:
-            # return url_for('film', imdb=film_imdb)
-            return render_template('film.html', film_imdb=film_imdb, film_name_en=film_name_en)
-    return render_template('index.html')
 
 def get_login():
     if request.method == 'GET':
         return render_template('login.html')
     else:
-        username = request.form['username']
+        user_name = request.form['user_name']
         password = request.form['password']
-        if len(username) == 0 | len(password) == 0:
+        if len(user_name) == 0 | len(password) == 0:
             return render_template('login.html')
 
         cursor.execute('select user_name from film.user')
-        usernames = cursor.fetchall()
+        user_names = cursor.fetchall()
 
-        for user in usernames:
-            if request.form['username'] == user[0]:
+        for user in user_names:
+            if request.form['user_name'] == user[0]:
                 sql_pw = 'select user_password from film.user where user_name' + '=' + '%s'
                 cursor.execute(sql_pw, (user[0]))
 
@@ -50,8 +36,8 @@ def get_login():
 
                 if request.form['password'] == pw[0][0]:
                     # if request.form['password'] == pw:
-                    return render_template('index.html', user_name=username)
-
+                    # return render_template('index.html', user_name=user_name)
+                    # return url_for('index')
                 return '<h>账号密码错误！</h>'
         cursor.close()
         conn.close()
@@ -61,7 +47,7 @@ def get_register():
     if request.method == 'GET':
         return render_template('login.html')
     else:
-        user_name = request.form['username']
+        user_name = request.form['user_name']
         user_password = request.form['password']
         if len(user_name) == 0 | len(user_password) == 0:
             return render_template('register.html')
@@ -69,32 +55,127 @@ def get_register():
 
     return render_template("register.html")
 
-def get_film(imdb):
+
+def get_film():
+    film_name_ch = request.form['search_film']
+    sql_film_name = 'select * from film.film where film_name_ch' + '=' + '%s'
+    cursor.execute(sql_film_name, [film_name_ch])
+
+    film_infos = cursor.fetchall()
+
+    for film_info in film_infos:
+        film_imdb = film_info[0]
+        film_name_ch = film_info[1]
+        film_name_en = film_info[2]
+        description = film_info[3]
+
+    sql_film_cast = 'select * from film.cast where imdb' + '=' + '%s'
+    cursor.execute(sql_film_cast, [film_imdb])
+    film_casts = cursor.fetchall()
+
+    # for film_cast in film_casts:
+    #     cast_types = film_cast[2]
+    #     person_names = film_cast[3]
+    cast_types = [film_cast[2] for film_cast in film_casts]
+    person_names = [film_cast[3] for film_cast in film_casts]
+
+    if film_imdb:
+        # get_cast(film_imdb)
+        return render_template('film.html', film_imdb=film_imdb, film_name_en=film_name_en,
+                               description=description, cast_types=cast_types, person_names=person_names)
+
+
+def get_imdb(film_name_ch):
+    sql_film_imdb = 'select imdb from film.film where film_name_ch' + '=' + '%s'
+    cursor.execute(sql_film_imdb, [film_name_ch])
+    film_imdb = cursor.fetchone()
+    return film_imdb
+
+
+def get_cast(film_imdb):
+    # film_imdb = get_imdb(film_name_ch)
+
+    sql_film_cast = 'select * from film.cast where imdb' + '=' + '%d'
+    cursor.execute(sql_film_cast, film_imdb)
+    film_casts = cursor.fetchall()
+
+    for film_cast in film_casts:
+        cast_type = film_cast[2]
+        person_name = film_cast[3]
+
+    if film_imdb:
+        # return url_for('film', imdb=film_imdb)
+        print(person_name)
+        return render_template('film.html', person_name=person_name, cast_type=cast_type)
+
+
+def get_comment():
     if request.method == 'GET':
-        return render_template('index.html')
+        return render_template('comment_new.html')
     else:
-        # 中文查找是什么电影
-        film_name = request.form['search_film']
-        sql_film_name = 'select * from film.film where film_name_ch' + '=' + '%s'
-        cursor.execute(sql_film_name, [film_name])
+        return render_template("comment_new.html")
 
-        film_infos = cursor.fetchall()
 
-        for film_info in film_infos:
-            film_imdb = film_info[0]
-            film_name_ch = film_info[1]
-            film_name_en = film_info[2]
+def get_comments():
+    if request.method == 'GET':
 
-        if film_imdb:
-            # return url_for('film', imdb=film_imdb)
-            return render_template('film.html', film_imdb=film_imdb, film_name_en=film_name_en)
+        return render_template('comments.html')
+    else:
+        comment_id=request.form['Comment_ID']
+        imdb=request.form['IMDB']
+        user_id=request.form['user']
+        comment_content=request.form['Comment']
+        comment_time=request.form['Time']
+
+        # 插入数据
+        sql_insert_comment = 'insert into film.comment values(' + '%s' + ', %s' + ', %s' + ', %s' + ', %s'')'
+        try:
+            cursor.execute(sql_insert_comment, [comment_id, imdb, user_id, comment_content, comment_time])
+            # 提交
+            conn.commit()
+        except Exception as e:
+            conn.rollback()
+    return render_template("comments.html", comment_id=comment_id, imdb=imdb, user_id=user_id,
+                           comment_content=comment_content, comment_time=comment_time)
+
+
+def get_likes():
+    # 更新点赞的数据
+    sql_update_film = "update film.Cast set person_name = '%s' where person_id = %d"
+    sql_update_film2= "update film.Cast set cast_type = '%s' where person_id = %d "
+
+    try:
+        cursor.execute(sql_update_film % ("Nobody", 2))
+        cursor.execute(sql_update_film2 % ("actor", 2))
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+
+# 还没做完
+def get_user():
+    author=request.form['user_id']
+    cursor.execute('select user_name from film.user')
+    user_names = cursor.fetchall()
+
+    for user in user_names:
+        if request.form['user_name'] == user[0]:
+            sql_pw = 'select user_password from film.user where user_name' + '=' + '%s'
+            cursor.execute(sql_pw, (user[0]))
+
+            pw = cursor.fetchall()
+
+            if request.form['password'] == pw[0][0]:
+                # if request.form['password'] == pw:
+                return render_template('index.html', user_name=user_name)
+
+            return '<h>账号密码错误！</h>'
 
 # 插入操作
 def insert_user(user_name, user_password):
     sql_insert_user = 'insert into film.user values(' + '%s' + ', %s' + ')'
     try:
         cursor.execute(sql_insert_user, [user_name, user_password])
-        #     提交
+        # 提交
         conn.commit()
     except Exception as e:
         conn.rollback()
--- views.py
+++ views.py
@@ -32,51 +32,67 @@ def register():
     return register_data
 
 
-@app.route("/films/<int:imdb>", methods=["GET", "POST"])
+@app.route("/films", methods=["GET", "POST"])
 def film():
     film_data = logic.get_film()
     return film_data
 
 
+@app.route("/comment/new", methods=["GET", "POST"])
+def comment():
+    comment_data = logic.get_comment()
+    return comment_data
 
 
-    # 查询操作
-    # sql = "SELECT * FROM Cast"
-    # try:
-    #     cursor.execute(sql)
-    #
-    #     results = cursor.fetchall()
-    #
-    #     for row in results:
-    #         person_name = row[3]
-    #         cast_type = row[2]
-    #         # print("Big boy is %s, %s." % (person_name, cast_type))
-    #         return render_template("index.html", person_name=person_name)
-    # except:
-    #     print("Error!")
-    # cursor.close()
+@app.route("/comment/asdakbdjaksssknd", methods=["GET", "POST"])
+def comments():
+    comments_data = logic.get_comments()
+    return comments_data
 
 
-    # 更新操作
-    # sql_update_film = "update film.Cast set person_name = '%s' where person_id = %d"
-    # sql_update_film2= "update film.Cast set cast_type = '%s' where person_id = %d "
-    #
-    # try:
-    #     cursor.execute(sql_update_film % ("Nobody", 2))
-    #     cursor.execute(sql_update_film2 % ("actor", 2))
-    #     conn.commit()
-    # except Exception as e:
-    #     conn.rollback()
+@app.route("/comment/asdakbdjaksssknd", methods=["GET", "POST"])
+def likes():
+    likes_data = logic.get_likes()
+    return likes_data
 
-    # 删除操作
-    # sql_delete= "delete from film.Cast where person_id = %d"
-    #
-    # try:
-    #     cursor.execute(sql_delete % (2))
-    #     conn.commit()
-    # except Exception as e:
+@app.route("/user", methods=["GET", "POST"])
+def user():
+    user_data = logic.get_user()
+    return user_data
 
+# 查询操作
+# sql = "SELECT * FROM Cast"
+# try:
+#     cursor.execute(sql)
+#
+#     results = cursor.fetchall()
+#
+#     for row in results:
+#         person_name = row[3]
+#         cast_type = row[2]
+#         # print("Big boy is %s, %s." % (person_name, cast_type))
+#         return render_template("index.html", person_name=person_name)
+# except:
+#     print("Error!")
+# cursor.close()
 
+# 更新操作
+# sql_update_film = "update film.Cast set person_name = '%s' where person_id = %d"
+# sql_update_film2= "update film.Cast set cast_type = '%s' where person_id = %d "
+#
+# try:
+#     cursor.execute(sql_update_film % ("Nobody", 2))
+#     cursor.execute(sql_update_film2 % ("actor", 2))
+#     conn.commit()
+# except Exception as e:
 #     conn.rollback()
 
+# 删除操作
+# sql_delete= "delete from film.Cast where person_id = %d"
+#
+# try:
+#     cursor.execute(sql_delete % (2))
+#     conn.commit()
+# except Exception as e:
 
+#     conn.rollback()
